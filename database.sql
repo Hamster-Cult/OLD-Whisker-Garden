3ea8c0a85ba6021f353a2b12d921bb790f3cea5b
K_Vebere <vebere2204@gmail.com>
2025-02-13 17:23:06 +0000

    code code codeee

--- database.sql
+++ database.sql
@@ -1,1 +1,16 @@
+/*Chloe's code*/
 
+CREATE TABLE entries (
+    entry_id SERIAL PRIMARY KEY,
+    entry VARCHAR(255) NULL,
+    entry_date DATE NOT NULL,
+    entry_time TIME NOT NULL,
+    rating INT NULL CHECK (rating >= 0 AND rating <= 5)
+)
+
+CREATE TABLE user_entries (
+    user_id INT NOT NULL,
+    entry_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user(user_id),
+    FOREIGN KEY (entry_id) REFERENCES entries(entry_id)
+)
