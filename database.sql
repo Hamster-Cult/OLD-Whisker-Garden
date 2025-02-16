162abb16e92f2c71fe65b88fc9a75631b511812a
Chloeluke <158167628+Chloeluke@users.noreply.github.com>
2025-02-16 20:45:06 +0000

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,4 +1,27 @@
 /*Chloe's code*/
+CREATE TABLE user (
+    user_id SERIAL PRIMARY KEY,
+    plant_id INT NOT NULL,
+    username VARCHAR(20) NOT NULL,
+    level INT NOT NULL CHECK (level >= 0),
+    exp INT NOT NULL CHECK (exp >= 0)
+);
+
+CREATE TABLE mood_log (
+    mood_id SERIAL PRIMARY KEY,
+    mood_date DATE NOT NULL CHECK (mood_date <= CURRENT_DATE),
+    mood_time TIME NOT NULL,
+    mood_rating SMALLINT NOT NULL CHECK (mood_rating BETWEEN 1 AND 5),
+    mood_text TEXT NULL
+);
+
+CREATE TABLE goals (
+    goal_id SERIAL PRIMARY KEY,
+    plant_id INT NOT NULL,
+    goal_name VARCHAR(50) NOT NULL,
+    goal_desc TEXT NULL,
+    goal_achievement BOOLEAN NOT NULL
+);
 
 CREATE TABLE entries (
     entry_id SERIAL PRIMARY KEY,
